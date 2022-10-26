77ea4c8da5f821d88c33588848f6cb3c048d26c1
pablojlpk <113646557+pablojlpk@users.noreply.github.com>
2022-10-26 08:00:51 -0300

    Update UniversidadG7.java

--- src/Vistas/UniversidadG7.java
+++ src/Vistas/UniversidadG7.java
@@ -27,32 +27,68 @@ public class UniversidadG7 {
 
         //Conexion con = new Conexion();
         Alumno nuevo = new Alumno();
-        
+
         nuevo.setApellido("SANCHEZ");
         nuevo.setNombre("TOMAS JOAQUIN");
-        nuevo.setDni(4444);
+        nuevo.setDni(44444444);
+//se dan de alta alumnos en bd
+        nuevo.setApellido("Lopez Pujato");
+        nuevo.setNombre("Pablo Javier");
+        nuevo.setDni(11111111);
         nuevo.setFecha_nacimiento(LocalDate.now());
         nuevo.setEstado(true);
-      dat.guardarAlumno(nuevo);
- 
+        System.out.println("Envia alumno " + nuevo.getApellido());
+        dat.guardarAlumno(nuevo);//se realiza conexion y se envia datos
+
+        nuevo.setApellido("Seitz");
+        nuevo.setNombre("Pablo Javier");
+        nuevo.setDni(22222222);
+        nuevo.setFecha_nacimiento(LocalDate.now());
+        nuevo.setEstado(true);
+        System.out.println("Envia alumno " + nuevo.getApellido());
+        dat.guardarAlumno(nuevo);//se realiza conexion y se envia datos
+
+        nuevo.setApellido("Sanchez");
+        nuevo.setNombre("Tomas");
+        nuevo.setDni(33333332);
+        nuevo.setFecha_nacimiento(LocalDate.now());
+        nuevo.setEstado(true);
+//        dat.guardarAlumno(nuevo);
+
+        System.out.println("Envia alumno " + nuevo.getApellido());
+        dat.guardarAlumno(nuevo);//se realiza conexion y se envia datos
+
 //BUSCANDO ALUMNO POR ID///
-System.out.println(dat.buscarAlumno(3));
+        System.out.println(dat.buscarAlumno(3));
         System.out.println(dat.buscarAlumno(1));
-         
-        
+        System.out.println("BUSCA POR UN ID INCORRECTO");
+        System.out.println(dat.buscarAlumno(3));//id existente
+        System.out.println("BUSCA POR UN ID CORRECTO");
+        System.out.println(dat.buscarAlumno(1));
+
         ///ACTUALIZANDO ALUMNO POR DNI
         nuevo.setId_alumno(6);
         nuevo.setApellido("PP");
-        
-       // dat.actualizarAlumno(nuevo);
+
+        ///ACTUALIZANDO ALUMNO POR CODIGO ID ALUMNO=1
+        nuevo.setId_alumno(1);
+        nuevo.setApellido("LOPEZ");
+        nuevo.setNombre("RUBEN");
+        nuevo.setDni(25555555);
+        nuevo.setFecha_nacimiento(LocalDate.now());
+        nuevo.setEstado(true);
+        System.out.println("actualiza datos por id = 1");
+        dat.actualizarAlumno(nuevo);
+
+        // dat.actualizarAlumno(nuevo);
         // AGREGAR MATERIA
         Materia ma = new Materia();
         ma.setNombre("MAtematica 1");
         ma.setAnio(1);
         ma.setEstado(true);
-        
+
         MateriaData m = new MateriaData();
         m.guardarMateria(ma);
-        
+
     }
 }
