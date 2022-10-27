f551120e6352747cf1cc02b847d5b4672767b1b1
TommySanchez12 <tomyjoaqo@gmail.com>
2022-10-27 19:10:39 -0300

    Update MateriaData.java

--- src/persistencia/MateriaData.java
+++ src/persistencia/MateriaData.java
@@ -6,15 +6,11 @@
 package persistencia;
 
 import Entidades.Materia;
-import Vistas.UniversidadG7;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
